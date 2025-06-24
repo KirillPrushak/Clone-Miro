1f43baf3cf434cdb2d4a54d326f8ec965130fbd7
KirillPrushak <kirillprushak@gmail.com>
2025-06-24 23:52:16 +0300

    setum router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,17 @@
+import { Outlet, useLocation } from "react-router-dom";
+import { AppHeader } from "@/features/header";
+import { ROUTES } from "@/shared/model/routes";
+
 export function App() {
-  return <div>App</div>;
+  const location = useLocation();
+
+  const isAuthPage =
+    location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
+
+  return (
+    <div>
+      {!isAuthPage && <AppHeader />}
+      <Outlet />
+    </div>
+  );
 }
